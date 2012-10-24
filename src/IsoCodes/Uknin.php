50478b24ee8d407806b033846aa4b4b4f3d361a2
Ronan Guilloux <ronan.guilloux@gmail.com>
2012-10-24 22:02:08 +0200

    php-checkstyle (psr2) over Uknin class

--- src/IsoCodes/Uknin.php
+++ src/IsoCodes/Uknin.php
@@ -4,18 +4,19 @@ namespace IsoCodes;
 
 class Uknin implements IsoCodeInterface
 {
-	/**
-	 * UK's National Insurance number validator
-	 *
-	 * @author ronan.guilloux
-	 * @param string $uknin
+    /**
+     * UK's National Insurance number validator
+     *
+     * @author ronan.guilloux
+     * @param string $uknin
      * @see http://www.govtalk.gov.uk/gdsc/html/frames/NationalInsuranceNumber-2-1-Release.htm
-	 * @return boolean
-	 */
-	public static function validate( $uknin )
-	{
+     * @return boolean
+     */
+    public static function validate( $uknin )
+    {
         $regexpMustCheck = "/^[A-CEGHJ-NOPR-TW-Z]{1}[A-CEGHJ-NPR-TW-Z]{1}[0-9]{6}[A-D\s]{1}/i";
         $regexpMustNOTCheck = "/(^GB)|(^BG)|(^NK)|(^KN)|(^TN)|(^NT)|(^ZZ).+/i";
-        return (boolean)(preg_match( $regexpMustCheck, $uknin ) && !preg_match( $regexpMustNOTCheck, $uknin ) );
-	}
+
+        return (boolean) (preg_match( $regexpMustCheck, $uknin ) && !preg_match( $regexpMustNOTCheck, $uknin ) );
+    }
 }
