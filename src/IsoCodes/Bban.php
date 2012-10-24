42c8312b5f8a900a31bc9c58c6616c5d9fa9702a
Ronan Guilloux <ronan.guilloux@gmail.com>
2012-10-24 22:06:30 +0200

    php-checkstyle (psr2) over Bban class

--- src/IsoCodes/Bban.php
+++ src/IsoCodes/Bban.php
@@ -4,31 +4,30 @@ namespace IsoCodes;
 
 class Bban implements IsoCodeInterface
 {
-	/**
-	 * Bban validator
-	 *
-	 * @author petitchevalroux
-	 * @licence originale http://creativecommons.org/licenses/by-sa/2.0/fr/
-	 * @link http://dev.petitchevalroux.net/php/valider-bban-php.359.html
-	 * @link http://fr.wikipedia.org/wiki/Relev%C3%A9_d%27identit%C3%A9_bancaire
-	 * @param string $bban
-	 * @return boolean
-	 */
-	public static function validate($bban)
-	{
-		if(mb_strlen($bban) !== 23)
-		{
-			return false;
-		}
-		$key = substr($bban,-2);
-		$bank = substr($bban,0,5);
-		$bank = substr($bban,0,5);
-		$branch = substr($bban,5,5);
-		$account = substr($bban,10,11);
-		$account = strtr($account,
-		'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-		'12345678912345678923456789');
-		return 97 - bcmod(89*$bank + 15 * $branch + 3 * $account,97) === (int)$key;
-	}
-}
+    /**
+     * Bban validator
+     *
+     * @author petitchevalroux
+     * @licence originale http://creativecommons.org/licenses/by-sa/2.0/fr/
+     * @link http://dev.petitchevalroux.net/php/valider-bban-php.359.html
+     * @link http://fr.wikipedia.org/wiki/Relev%C3%A9_d%27identit%C3%A9_bancaire
+     * @param  string  $bban
+     * @return boolean
+     */
+    public static function validate($bban)
+    {
+        if (mb_strlen($bban) !== 23) {
+            return false;
+        }
+        $key = substr($bban,-2);
+        $bank = substr($bban,0,5);
+        $bank = substr($bban,0,5);
+        $branch = substr($bban,5,5);
+        $account = substr($bban,10,11);
+        $account = strtr($account,
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        '12345678912345678923456789');
 
+        return 97 - bcmod(89*$bank + 15 * $branch + 3 * $account,97) === (int) $key;
+    }
+}
