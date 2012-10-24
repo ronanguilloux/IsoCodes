45ecba3b4ccc5cd5fd8e5f78dfd1c839e378b630
Ronan Guilloux <ronan.guilloux@gmail.com>
2012-10-24 22:05:46 +0200

    php-checkstyle (psr2) over SwiftBic class

--- src/IsoCodes/SwiftBic.php
+++ src/IsoCodes/SwiftBic.php
@@ -4,19 +4,20 @@ namespace IsoCodes;
 
 class SwiftBic implements IsoCodeInterface
 {
-	/**
-	 * SWIFT-BIC validator
+    /**
+     * SWIFT-BIC validator
      *
      * variant BIC Regex : ([a-zA-Z]{4}[a-zA-Z]{2}[a-zA-Z0-9]{2}([a-zA-Z0-9]{3})?)
-	 *
-	 * @author ronan.guilloux
-	 * @link http://networking.mydesigntool.com/viewtopic.php?tid=301&id=31
-	 * @param string $swiftbic
-	 * @return boolean
-	 */
-	public static function validate( $swiftbic )
-	{
+     *
+     * @author ronan.guilloux
+     * @link http://networking.mydesigntool.com/viewtopic.php?tid=301&id=31
+     * @param  string  $swiftbic
+     * @return boolean
+     */
+    public static function validate( $swiftbic )
+    {
         $regexp = "/^([a-zA-Z]){4}([a-zA-Z]){2}([0-9a-zA-Z]){2}([0-9a-zA-Z]{3})?$/";
-        return (boolean)preg_match( $regexp, $swiftbic );
-	}
+
+        return (boolean) preg_match( $regexp, $swiftbic );
+    }
 }
