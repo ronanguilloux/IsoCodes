94e944e955e6ba0e8a4c2c7741c4ac271b1455a9
Ronan Guilloux <ronan.guilloux@gmail.com>
2012-10-24 22:02:47 +0200

    php-checkstyle (psr2) over IP class

--- src/IsoCodes/IP.php
+++ src/IsoCodes/IP.php
@@ -8,10 +8,11 @@ class IP implements IsoCodeInterface
      * IPV4 public-only validator
      *
      * @link http://php.net/manual/fr/function.filter-var.php
-     * @param string $ip
+     * @param  string  $ip
      * @return boolean
      */
-    public static function validate($ip){
+    public static function validate($ip)
+    {
         return (false != filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4));
     }
 
@@ -19,10 +20,11 @@ class IP implements IsoCodeInterface
      * IPV6 validator
      *
      * @link http://php.net/manual/fr/function.filter-var.php
-     * @param string $ip
+     * @param  string  $ip
      * @return boolean
      */
-    public static function validateIPV6($ip){
+    public static function validateIPV6($ip)
+    {
         return (false != filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6));
 
     }
