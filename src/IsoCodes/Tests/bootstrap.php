26d22781b215b9c0b9e7698e87471711964a0644
Ronan Guilloux <ronan.guilloux@gmail.com>
2013-02-10 17:15:11 +0100

    tests/ siblinged to src/

--- src/IsoCodes/Tests/bootstrap.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-require_once __DIR__.'/../../../vendor/autoload.php';
-setlocale(LC_ALL, 'fr_FR');
---
+++ tests/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+setlocale(LC_ALL, 'fr_FR');
+
+$vendorDir = __DIR__ . '/../vendor';
+
+if (!@include($vendorDir . '/autoload.php')) {
+        die("You must set up the project dependencies, run the following commands:
+            wget http://getcomposer.org/composer.phar
+            php composer.phar install
+            ");
+}
+
+$loader = require $vendorDir .  '/autoload.php';
+$loader->add('IsoCodes\\Tests', __DIR__);
