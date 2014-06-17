fb7bd0a1666d4504fbda22fa3cb6e9aefddee076
Ronan Guilloux <ronan.guilloux@gmail.com>
2014-06-17 11:05:26 +0200

    Update FranceTest.php

--- tests/IsoCodes/Tests/ZipCodes/FranceTest.php
+++ tests/IsoCodes/Tests/ZipCodes/FranceTest.php
@@ -23,6 +23,7 @@ class FranceTest extends \PHPUnit_Framework_TestCase
             array( '06000',     'France', true ),
             array( '56000',     'France', true ),
             array( '56420',     'France', true ),
+            array( '20000',     'France', true ), // Ajaccio: ZIP code (20000,20090) is not an INSEE code
             array( '97114',     'France', true ), // 971 - 14, Trois-Rivières, Guadeloupe
             array( '99999',     'France', true ), // Service consommateurs
             array( '99123',     'France', true ), // Paris - Concours
@@ -32,6 +33,7 @@ class FranceTest extends \PHPUnit_Framework_TestCase
             array( '01000',     'france', true ),
             array( '01000',     'FRANCE', true ),
             //bad:
+            array( '2A004',     'France', false ), // Ajaccio: INSEE code (2A004) is not a ZIP code (20000,20090)
             array( '560',       'France', false ),
             array( '5600',      'France', false ),
             array( '560000',    'France', false ),
