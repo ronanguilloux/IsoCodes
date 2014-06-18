25a5c5ba8309b63ea56f0300246f3cb39becf7d1
Ronan Guilloux <ronan.guilloux@gmail.com>
2014-06-18 10:04:01 +0200

    Code style matters

--- tests/IsoCodes/Tests/CreditCardTest.php
+++ tests/IsoCodes/Tests/CreditCardTest.php
@@ -23,23 +23,23 @@ class CreditCardTest extends \PHPUnit_Framework_TestCase
     {
         return array(
             //good:
-            array('340000000000009',    true ), //American Express
-            array('30000000000004',     true ), //Carte Blanche
-            array('6011000000000004',   true ), //Discover
-            array('38520000023237',     true ), //DinersClub
-            array('201400000000009',    true ), //enRoute
-            array('2131000000000008',   true ), //JCB
-            array('5500000000000004',   true ), //MasterCard
-            array('6334000000000004',   true ), //Solo
-            array('4903010000000009',   true ), //Switch
-            array('4111111111111111',   true ), //Visa
-            array('6304100000000008',   true ), //Laser
-            array(6304100000000008,   true ), //Laser
+            array( '340000000000009',    true ), //American Express
+            array( '30000000000004',     true ), //Carte Blanche
+            array( '6011000000000004',   true ), //Discover
+            array( '38520000023237',     true ), //DinersClub
+            array( '201400000000009',    true ), //enRoute
+            array( '2131000000000008',   true ), //JCB
+            array( '5500000000000004',   true ), //MasterCard
+            array( '6334000000000004',   true ), //Solo
+            array( '4903010000000009',   true ), //Switch
+            array( '4111111111111111',   true ), //Visa
+            array( '6304100000000008',   true ), //Laser
+            array( 6304100000000008,     true ), //Laser
             //bad:
-            array('CE1EL2LLFFF',        false ),
-            array('E31DCLLFFF',         false ),
-            array('',                   false ),
-            array(' ',                  false )
+            array( 'CE1EL2LLFFF',        false ),
+            array( 'E31DCLLFFF',         false ),
+            array( '',                   false ),
+            array( ' ',                  false )
         );
     }
 
