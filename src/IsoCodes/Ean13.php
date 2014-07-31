fc3cff9899b9faee2a5e20ec3e6866bae3855d58
Ronan Guilloux <ronan.guilloux@gmail.com>
2014-07-31 08:02:48 +0200

    cs fixing

--- src/IsoCodes/Ean13.php
+++ src/IsoCodes/Ean13.php
@@ -11,14 +11,18 @@ class Ean13 implements IsoCodeInterface
 {
 
     /**
-     * @param $ean13
+     * @param mixed $ean13
      *
      * @return bool
      */
     public static function validate($ean13)
     {
-        if (strlen($ean13) != 13) return false;
-        if (!is_numeric($ean13)) return false;
+        if (strlen($ean13) != 13) {
+            return false;
+        }
+        if (!is_numeric($ean13)) {
+            return false;
+        }
 
         // The weight for a specific position in the EAN code is either 3 or 1,
         // which alternate so that the final data digit has a weight of 3;
@@ -26,13 +30,19 @@ class Ean13 implements IsoCodeInterface
         $sum = 0;
         for ($index = 0; $index < 12; $index++) {
             $number = (int) $ean13[$index];
-            if (($index % 2) != 0) $number *= 3;
+            if (($index % 2) != 0) {
+                $number *= 3;
+            }
             $sum += $number;
         }
 
         $key = $ean13[12]; // Ean13's checksum digit key
 
         // The Check digit, a single checksum digit, is computed modulo 10
-        if (10 - ($sum % 10) != $key) return false; else return true;
+        if (10 - ($sum % 10) != $key) {
+            return false;
+        }
+
+        return true;
     }
 }
