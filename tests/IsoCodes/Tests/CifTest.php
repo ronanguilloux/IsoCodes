6fc81c7e53aae64c2af622a910cb83c50de74616
Ronan Guilloux <ronan.guilloux@gmail.com>
2014-10-27 14:53:23 +0100

    more data in test providers

--- tests/IsoCodes/Tests/CifTest.php
+++ tests/IsoCodes/Tests/CifTest.php
@@ -13,7 +13,7 @@ class CifTest extends \PHPUnit_Framework_TestCase
 {
 
     /**
-     * getValidCifs: dataprovider
+     * getValidCifs: data provider
      *
      * @link http://www.linguee.fr/anglais-francais/traduction/cif+number.html
      *
@@ -22,22 +22,29 @@ class CifTest extends \PHPUnit_Framework_TestCase
     public function getValidCifs()
     {
         return array(
-            array('N0032484H'),
+            array('N0032484H'), // non-resident
             array('A09212275'),
-            array('A59032557'),
+            array('A59032557'), // Public companies, “Sociedades Anonimas” or ‘SA’s
             array('A17031246'),
-            array('B85358596'),
+            array('B85358596'), // ‘Sociedades de Responsibilidad Limitada’ or ‘SL’s’
+            array('E61685095'), // Business Partnerships, ‘Communidades de Bienes’ or ‘CB’s’
+            array('V27236942'),
+            array('G61685095'),
         );
     }
 
     /**
-     * getValidCifs: dataprovider
+     * getValidCifs: data provider
      *
      * @return array
      */
     public function getInvalidCifs()
     {
         return array(
+            array('K11111111'), // Spanish children under the age of 14 who need a fiscal number
+            array('L61685095'), // Spanish citizens resident outside Spain who do not have a Spanish Identity Card
+            array('X61685095'), // Foreign individuals with financial interests in Spain < 15/07/2008
+            array('Y61685095'), // Foreign individuals with financial interests in Spain > 15/07/2008
             array('N0032484'),      // no end control
             array('N0032484I'),     // NIF: first digit OK, end control digit KO
             array('M0032484I'),     // NIF: first digit KO, end control digit KO
@@ -76,9 +83,11 @@ class CifTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse(Cif::validate($cif));
     }
 
+    /**
+     * {@inheritdoc}
+     */
     protected function setUp()
     {
         parent::setUp();
     }
-
 }
