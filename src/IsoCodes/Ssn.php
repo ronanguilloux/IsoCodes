e06598fc42afed38b0d2aad5d7c3abe90a7e0d53
Ronan Guilloux <ronan.guilloux@gmail.com>
2016-01-06 13:41:41 +0100

    fix #80 - deprecated in 1.3 become abandoned in 2.*

--- src/IsoCodes/Ssn.php
+++ src/IsoCodes/Ssn.php
@@ -216,11 +216,6 @@ EOT;
     // The SSA uses a funky method of figuring out what group number to use next. This area has them in the proper order and makes it easier to generate a SSN.
     public static $possibleGroups = array(1, 3, 5, 7, 9, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 2, 4, 6, 8, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99);
 
-    public function __construct()
-    {
-        @trigger_error('Instantiating Ssn validator is deprecated since version 1.3. Use Ssn::validate($ssn) instead.', E_USER_DEPRECATED);
-    }
-
     /**
      * Cleans the high group number list so it is useful.
      */
