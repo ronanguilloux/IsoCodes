53509862747cdfc626d4ceb999c12adba2f4c9b7
Ronan Guilloux <ronan.guilloux@gmail.com>
2020-12-27 17:24:32 +0100

    updating to 7.3 as 7.2 is unsupported

--- .php_cs.php
+++ .php_cs.php
@@ -3,6 +3,14 @@ return PhpCsFixer\Config::create()
     ->setRules(array(
         '@PSR2' => true,
         '@Symfony' => true,
+        'constant_case' => true,
+        'lowercase_keywords' => true,
+        'native_function_casing' => true,
+        'native_function_type_declaration_casing' => true,
+        'class_attributes_separation' => true,
+        'class_definition' => true,
+        'visibility_required' => true,
+        'no_blank_lines_after_class_opening' => true,
         'linebreak_after_opening_tag' => true,
         'ordered_imports' => true,
         'method_argument_space' => [
