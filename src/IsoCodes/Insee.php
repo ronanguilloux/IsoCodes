8c0d1639a93c6064d7247df1c13d2bd4672ae3c6
Johan Desmyter <johan@etic.yt>
2021-05-19 13:56:13 +0300

    fix Insee validation for month part

--- src/IsoCodes/Insee.php
+++ src/IsoCodes/Insee.php
@@ -26,7 +26,7 @@ class Insee implements IsoCodeInterface
         $regexp = '/^                                              # début de chaîne
             (?<sexe>[123478])                                      #  1 pour les hommes, 2 pour les femmes, 3 ou 7 pour les personnes étrangères de sexe masculin en cours d\'immatriculation en France, 4 ou 8 pour les personnes étrangères de sexe féminin en cours d\'immatriculation en France
             (?<annee>[0-9]{2})                                     # année de naissance
-            (?<mois>0[1-9]|1[0-2]|3[1-9]|4[0-2]|[5-9][0-9]|20)     # mois de naissance: de 01 (janvier) à 12 (décembre) ou entre 30 et 42 ou entre 50 et 99 ou égal à 20 pour un mois non connu
+            (?<mois>0[1-9]|1[0-2]|[2-3][0-9]|4[0-2]|[5-9][0-9])    # mois de naissance: de 01 (janvier) à 12 (décembre) ou entre 20 et 42 ou entre 50 et 99
                     (?<departement>[0][1-9]|2[AB]|[1-9][0-9])      # le département : de 01 à 95, ou 2A ou 2B pour la Corse après le 1er janvier 1976, ou 96 à 98 pour des naissances hors France métropolitaine et 99 pour des naissances à l\'étranger. Attention, cas particuliers supplémentaire outre-mer traité plus loin, hors expreg
                     (?<numcommune>[0-9]{3})                        # numéro d\'ordre de la commune (attention car particuler pour hors métro  traité hors expression régulière)
                     (?<numacte>00[1-9]|0[1-9][0-9]|[1-9][0-9]{2})  # numéro d\'ordre d\'acte de naissance dans le mois et la commune ou pays
@@ -50,7 +50,7 @@ class Insee implements IsoCodeInterface
         $return = [
             'sexe' => $match['sexe'], //7,8 => homme et femme ayant un num de sécu temporaire
             'annee' => $match['annee'], //année de naissance + ou - un siècle uhuh
-            'mois' => $match['mois'], //20 = inconnu
+            'mois' => $match['mois'], //20 à 30 ou 50 à 99 = inconnu
             'departement' => $match['departement'], //99 = étranger
             'numcommune' => $match['numcommune'], //990 = inconnu
             'numacte' => $match['numacte'], //001 à 999
